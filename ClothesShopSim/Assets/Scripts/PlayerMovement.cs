95bf77e27db17c884f68903b0ac218fa1c986bd4
nacholines <nacho.lines1@gmail.com>
2023-05-19 10:45:32 -0300

    First commit + added player and player movement

---
+++ ClothesShopSim/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MovementSpeed = 5f;
+    public KeyCode[] Controls;
+    private Vector3 _movement;
+    public Rigidbody2D Rb;
+
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        _movement = Vector3.zero;
+
+        if (Input.GetKey(Controls[0]))
+        {
+            _movement += Vector3.up;
+        }
+
+        if (Input.GetKey(Controls[1]))
+        {
+            _movement += Vector3.down;
+        }
+
+        if (Input.GetKey(Controls[2]))
+        {
+            _movement += Vector3.left;
+        }
+
+        if (Input.GetKey(Controls[3]))
+        {
+            _movement += Vector3.right;
+        }
+    }
+}
